9b9d9b5613cd34b529985f39dcf5fc43242a55b3
罗锦旭 <luojinxu@linklogis.com>
2021-03-17 18:22:26 +0800

    feat: 同步

--- js/door.js
+++ js/door.js
@@ -8,6 +8,33 @@ const stats = new Stats(); // 性能监控器，用来查看Three.js渲染帧率
 const container = document.createElement('div');
 document.body.appendChild(container);
 
+//暂存helper类
+class AxisGridHelper {
+  constructor(node, units = 10) {
+    const axes = new THREE.AxesHelper();
+    axes.material.depthTest = false;
+    axes.renderOrder = 2; // 在网格渲染之后再渲染
+    node.add(axes);
+
+    const grid = new THREE.GridHelper(units, units);
+    grid.material.depthTest = false;
+    grid.renderOrder = 1;
+    node.add(grid);
+
+    this.grid = grid;
+    this.axes = axes;
+    this.visible = false;
+  }
+  get visible() {
+    return this._visible;
+  }
+  set visible(v) {
+    this._visible = v;
+    this.grid.visible = v;
+    this.axes.visible = v;
+  }
+}
+
 // 创建场景
 const scene = new THREE.Scene();
 
